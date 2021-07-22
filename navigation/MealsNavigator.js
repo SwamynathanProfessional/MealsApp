2eef44898a5ef66790762e1ac77e048d32917da3
SwamynathanProfessional <swamynathan.bommaganty@gmail.com>
2021-07-22 15:41:39 +0530

    uu

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,16 +1,41 @@
+import {Platform} from 'react-native';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-  Categories: {
-    screen: CategoriesScreen,
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: {
+        headerStyle: {
+          headerTitle: 'Meals Categories',
+          backgroundColor: Platform.OS === 'android' ? Colors.accentColor : '',
+          headerTintColor:
+            Platform.OS === 'android' ? 'red' : Colors.primaryColor,
+        },
+      },
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen,
+      navigationOptions: {
+        headerStyle: {},
+      },
+    },
+
+    MealDetail: MealDetailScreen,
   },
-  CategoryMeals: CategoryMealsScreen,
-
-  MealDetail: MealDetailScreen,
-});
+  {
+    //initialRouteName: 'MealDetail',
+    defaultNavigationOptions: {
+      backgroundColor: Platform.OS === 'android' ? Colors.accentColor : '',
+      headerTintColor: Platform.OS === 'android' ? 'red' : Colors.primaryColor,
+      headerTitle: 'A screen',
+    },
+  },
+);
 
 export default createAppContainer(MealsNavigator);
